3799e8890fbfa17ef09ffb241bf3fad1d889b1de
Denis <korbakovd@gmail.com>
2025-08-18 15:22:35 +0300

    feat: ass ws route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\Api\EmailVerificationController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WSController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['throttle:limit'])->group(static function (): void {
@@ -17,6 +18,8 @@ Route::middleware(['throttle:limit'])->group(static function (): void {
 
     Route::middleware(['auth:sanctum'])->group(static function (): void {
         Route::post('/logout', [AuthController::class, 'logout']);
+
+        Route::get('/ws/auth', [WSController::class, 'auth'])->name('ws.auth');
 
         Route::group(['prefix' => 'emails'], static function (): void {
             Route::get('/send-verify-code', [EmailVerificationController::class, 'send']);
