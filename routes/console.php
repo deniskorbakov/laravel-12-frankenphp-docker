2633b6fbddf02d3ef15136432b5ffc343946b179
Denis <korbakovd@gmail.com>
2025-08-18 15:23:24 +0300

    feat: add new commnad

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Console\Commands\CentrifugoCommand;
 use App\Console\Commands\ExampleCommand;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(ExampleCommand::class)->everyFiveSeconds();
+Schedule::command(CentrifugoCommand::class)->everySecond();
