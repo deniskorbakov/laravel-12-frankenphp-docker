d123577ba7b6b272ddf6dd47af0595e3f39feaeb
Denis <korbakovd@gmail.com>
2025-08-24 15:18:33 +0300

    fix: delete extra command

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Console\Commands\CentrifugoCommand;
 use App\Console\Commands\ExampleCommand;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(ExampleCommand::class)->everyFiveSeconds();
-Schedule::command(CentrifugoCommand::class)->everySecond();
